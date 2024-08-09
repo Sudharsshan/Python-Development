02f900b6d63938fbd8e46b3d121c151e0733668d
Sudharsshan S Y <sudharsshansy@gmail.com>
2024-08-09 15:02:34 +0530

    GUI development using TKinter

---
+++ LabPrograms/GUI using TKinter/resistance_graph_using_matplotLib_TKinter.py
@@ -0,0 +1,10 @@
+# DATE: 08-08-2024
+# TASK: develop a python program to display a 
+#       graph of resistance increase with respect
+#       to time for given voltage and current
+#       from room temp (25*c) till 30*c
+
+import tkinter as tk
+import matplotlib.pyplot as plt
+import pandas as pd
+
